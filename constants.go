d42e919037f8c65d88e067bf5d8cf269c94212f7
Ivor Scott <ivor@devpie.io>
2024-06-28 22:31:44 +0200

    fix: comments

--- constants.go
+++ constants.go
@@ -18,15 +18,15 @@ const (
 	// MergeConflictBothSides means both the file in the current branch and the file being merged have conflicts.
 	MergeConflictBothSides = "UU"
 
-	// StagedAdded means an added file staged change
+	// StagedAdded means an added file staged change.
 	StagedAdded = "A "
-	// StagedModified means a modified file staged change
+	// StagedModified means a modified file staged change.
 	StagedModified = "M "
-	// StagedDeleted means a deleted file staged change
+	// StagedDeleted means a deleted file staged change.
 	StagedDeleted = "D "
-	// StagedRenamed means a renamed file staged change
+	// StagedRenamed means a renamed file staged change.
 	StagedRenamed = "R "
-	// StagedCopied means a copied file staged change
+	// StagedCopied means a copied file staged change.
 	StagedCopied = "C "
 
 	// MaxFetchBranchAttempts represents a maximum reties to fetch branches.
